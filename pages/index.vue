0d87ab6e9f343dbbab24cdcbf5dc5530c33b5db4
SavioHaistem <savio.dantas.5858@gmail.com>
2024-01-24 22:13:07 -0300

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="page">
+        <main class="accounts">
+            
+        </main>
+    </div>
+</template>
+<style scoped>
+div.page {
+    height: 100vh;
+    width: 100vw;
+    background: radial-gradient(rgb(221, 221, 221),rgb(180, 180, 180));
+}
+</style>
